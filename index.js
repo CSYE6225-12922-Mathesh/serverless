bd47abda880188cc05f025ce3cee9955546747c6
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-12-01 20:29:14 -0500

    KMS sendgrid

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+const AWS = require('aws-sdk');
 const dotenv = require("dotenv");
 const sendGridMail = require("@sendgrid/mail");
+const secretsManager = new AWS.SecretsManager();
+const secretName = process.env.SENDGRID_API_KEY_SECRET_NAME;
 
 dotenv.config();
 
-sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
+//sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async (event) => {
+  const secretValue = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+
+  let sendGridApiKey;
+
+  if (secretValue.SecretString) {
+    const secret = JSON.parse(secretValue.SecretString);
+    sendGridApiKey = secret.api_key; // Assuming your secret has a key named "api_key"
+  } else {
+    const buff = Buffer.from(secretValue.SecretBinary, 'base64');
+    sendGridApiKey = buff.toString('ascii');
+  }
+
+  // Set SendGrid API Key
+  sendGridMail.setApiKey(sendGridApiKey);
   const { email, verification_token, verification_token_expiration } = JSON.parse(
     event.Records[0].Sns.Message
   );
